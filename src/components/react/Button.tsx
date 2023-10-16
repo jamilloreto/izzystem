9f6b757f06e961c80adb6883c66840c3665d8a07
Jamil Loreto <jamil20002302@gmail.com>
2023-10-16 16:21:47 -0500

    adde button styles

--- src/components/react/Button.tsx
+++ src/components/react/Button.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Drawer } from "./Drawer";
+import s from "../../styles/components/Button.module.css";
 
 export function Button() {
   const [open, setOpen] = useState<boolean>(false);
@@ -10,24 +11,23 @@ export function Button() {
 
   return (
     <>
-      <div className="wrapButton">
-        <button className="button" onClick={handler}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="icon-xl"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-            />
-          </svg>
-        </button>
-      </div>
+      <button className={s.button} onClick={handler}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="icon-2xl"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+          />
+        </svg>
+      </button>
+
       {open && <Drawer handler={handler} />}
     </>
   );
